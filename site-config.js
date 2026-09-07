a08d836e846a68da5e86b3f3e86fca8a908a35fa
Apostolos Varelas <87095756+ApostolosVarelas@users.noreply.github.com>
2026-09-07 12:29:11 +0300

    updated video link

--- site-config.js
+++ site-config.js
@@ -14,7 +14,7 @@ window.SITE_CONFIG = {
       photoAlt: "Portrait of Apostolos Varelas"
     }
   ],
-  fullDemoUrl: "https://eagleprojectssrl-my.sharepoint.com/:v:/g/personal/avarelas_eagleprojects_world/IQA8DNFOml-iSKdToeS_zh5WAbs4u4oOSs_feDqh2miq3Ac?nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJPbmVEcml2ZUZvckJ1c2luZXNzIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXciLCJyZWZlcnJhbFZpZXciOiJNeUZpbGVzTGlua0NvcHkifX0&e=2TYyuM",
+  fullDemoUrl: "https://eagleprojectssrl-my.sharepoint.com/:v:/g/personal/avarelas_eagleprojects_world/IQA8DNFOml-iSKdToeS_zh5WAZ8uyXYRZ3tUTtG3EFrglOk?nav=eyJyZWZlcnJhbEluZm8iOnsicmVmZXJyYWxBcHAiOiJTdHJlYW1XZWJBcHAiLCJyZWZlcnJhbFZpZXciOiJTaGFyZURpYWxvZy1MaW5rIiwicmVmZXJyYWxBcHBQbGF0Zm9ybSI6IldlYiIsInJlZmVycmFsTW9kZSI6InZpZXcifX0%3D&e=f0GLyn",
   kpis: {
     missionCompletion: "20 / 20",
     humanIntervention: "0 / 20",
